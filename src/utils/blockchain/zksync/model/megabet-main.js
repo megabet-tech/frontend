cadfcdb51b6c21347dbd88ac0754c6c8caf7a55b
Albert Tran <alberttran0810@gmail.com>
2023-11-03 12:02:19 +0700

    Hot Fix: play function

--- src/utils/blockchain/zksync/model/megabet-main.js
+++ src/utils/blockchain/zksync/model/megabet-main.js
@@ -2,12 +2,18 @@ import { Provider } from "zksync-web3";
 import * as ethers from "ethers";
 import MegaBetMainContractAbi from "../../contracts/megabet-main";
 import config from "../config";
+import {
+    usePrepareContractWrite,
+    useContractWrite,
+    useWaitForTransaction,
+} from 'wagmi';
 
-let megaBetMainContract;
+let megaBetMainContract = null;
+let provider = null;
 
 export async function initMegaBetMainContract() {
     const deployMode = 'testnet';
-    const provider = new Provider(config[deployMode].network.url);
+    provider = new Provider(config[deployMode].network.url);
     megaBetMainContract = new ethers.Contract(
         config[deployMode].contract_address.megabet_main,
         MegaBetMainContractAbi.abi,
@@ -19,7 +25,24 @@ export async function play(betSessionId, luckyNumbers, mode, betAmount) {
     if (!megaBetMainContract) {
         return null;
     }
-    const overrides = {value: ethers.utils.parseEther("0.000000000001")}
-    const gasPrice = megaBetMainContract.estimateGas.play(betSessionId, luckyNumbers, mode, betAmount, overrides);
-    console.log(gasPrice.toString());
+    const deployMode = 'testnet';
+    const { config } = usePrepareContractWrite({
+        address: config[deployMode].contract_address.megabet_main,
+        abi: MegaBetMainContractAbi.abi,
+        functionName: 'play',
+        args: [betSessionId, luckyNumbers, mode, betAmount],
+    });
+
+    const { data, error, isError, write } = useContractWrite(config)
+    write({overrides: {value: ethers.utils.parseEther("0.01")} });
+    const { isLoading, isSuccess } = useWaitForTransaction({
+        hash: data?.hash,
+    })
+
+    
+    // console.log(megaBetMainContract);
+    // console.log(provider.getSigner());
+    // const overrides = {value: ethers.utils.parseEther("0.000000000001")}
+    // const gasPrice = megaBetMainContract.estimateGas.play(betSessionId, luckyNumbers, mode, betAmount);
+    // console.log(gasPrice.toString());
 }
